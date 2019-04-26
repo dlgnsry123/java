6194bd3bc7aa1456c99595536d313eb59a963938
USER <USER@AMP-20>
2019-04-26 10:21:33 +0900

    commit

---
+++ chap09_ex01/src/chap09_ex01/chap09_ex01.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class chap09_ex01 extends JFrame {
+	public chap09_ex01() {
+		setTitle("CA¡¤AO");
+		setSize(300, 300);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		chap09_ex01 frame = new chap09_ex01();
+
+	}
+
+}
